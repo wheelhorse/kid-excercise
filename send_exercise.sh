1c44745954373875bbc663ceb6869588556fcf84
wheelhorse <wufengbo348@gmail.com>
2023-05-24 17:15:13 +0800

    Added bcc support

--- send_exercise.sh
+++ send_exercise.sh
@@ -1,3 +1,3 @@
 #python3 genMath.py -s 10 -e 100 -m -n 5
 python3 genMath.py -s 1 -e 10 -n 5
-echo "Kid Exercise Math" |mail -s "10 Days Kid Math Exercise" -a "From:kidMath@cisco.com" -A "./MathTest.pdf" fengbwu@cisco.com ziyma@cisco.com
+echo "Kid Exercise Math" |mail -s "10 Days Kid Math Exercise" -a "From:kidMath@cisco.com" -a "Bcc:fengbwu@cisco.com" -A "./MathTest.pdf" ziyma@cisco.com
