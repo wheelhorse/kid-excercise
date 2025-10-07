b73b3ba19f68a042e236a34e69cdc97cb91eebc4
Fengbo Wu <fengbo_wu@163.com>
2025-10-07 08:42:38 +0000

    init base for test

--- send_exercise.sh
+++
@@ -1,3 +0,0 @@
-#python3 genMath.py -s 10 -e 100 -m -n 5
-python3 genMath.py -s 1 -e 10 -n 5
-echo "Kid Exercise Math" |mail -s "10 Days Kid Math Exercise" -a "From:kidMath@cisco.com" -a "Bcc:fengbwu@cisco.com" -A "./MathTest.pdf" ziyma@cisco.com
